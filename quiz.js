f0b85641a1a6c31e6960128e868cc6b7bc31a5a0
Godwin Nyavor <nyavorgodwin@gmail.com>
2025-11-30 15:34:42 +0000

    Styles

--- quiz.js
+++ quiz.js
@@ -2,7 +2,7 @@ function checkAnswer() {
     let correctAnswer = "4";
     let userAnswer = document.querySelector('input[name="quiz"]:checked');
 
-    if (userAnswer === correctAnswer) {
+    if (userAnswer.value === correctAnswer) {
         document.getElementById("feedback").textContent = "Correct! Well done."
     } else {
         document.getElementById("feedback").textContent = "That's incorrect. Try again!";
